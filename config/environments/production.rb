08e7307ab79e6f1d40f093ea88c691c61b669b8c
Abhinav Sahai <as5908@nyu.edu>
2012-03-24 01:31:04 -0400

    config.assets.precompile += %w( *.css *.js ) in config/production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,6 @@
 NewApp::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
+  config.assets.precompile += %w( *.css *.js )
 
   # Code is not reloaded between requests
   config.cache_classes = true
