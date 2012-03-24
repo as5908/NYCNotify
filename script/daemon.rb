fb8c6b6bbc7531e522c4733aca5b82ced7e7e3b5
Abhinav Sahai <as5908@nyu.edu>
2012-03-24 02:50:59 -0400

    daemon.rb and database.yml updated

--- script/daemon.rb
+++ script/daemon.rb
@@ -4,13 +4,27 @@ require 'twitter'
 require 'rubygems'
 require 'active_record'
 
+#require 'activerecord'
+require 'uri'
+
+db = URI.parse(ENV['DATABASE_URL'] || 'postgres://localhost/dbdevelopment')
+
 ActiveRecord::Base.establish_connection(
-  :adapter=>"postgresql",
-  :database => "db/development",
-  :host=>"localhost",
-  :username => "postgres",
-  :password => "root",
+  :adapter  => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
+  :host     => db.host,
+  :username => db.user,
+  :password => db.password,
+  :database => db.path[1..-1],
+  :encoding => 'utf8'
 )
+
+#ActiveRecord::Base.establish_connection(
+#  :adapter=>"postgresql",
+#  :database => "db/development",
+#  :host=>"localhost",
+#  :username => "postgres",
+#  :password => "root",
+#)
 
 class Authorization < ActiveRecord::Base
 end
