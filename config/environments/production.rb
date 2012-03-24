817a41e0d6774ed331770de41971f60bebbacace
Abhinav Sahai <as5908@nyu.edu>
2012-03-24 01:40:51 -0400

    updated production.rb 2

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -48,7 +48,7 @@ NewApp::Application.configure do
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   # config.assets.precompile += %w( search.js )
-  config.assets.precompile += %w( blueprint/screen.css blueprint/print.css )
+  config.assets.precompile += %w( blueprint/screen.css blueprint/print.css stylesheets/blueprint/screen.css stylesheets/blueprint/print.css)
 
 
   # Disable delivery errors, bad email addresses will be ignored
